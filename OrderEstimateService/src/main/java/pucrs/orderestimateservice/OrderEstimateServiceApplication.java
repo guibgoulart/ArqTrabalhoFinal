0d7da27eae72b2d6f276ac2e6786665c3e5d8d1e
guibg <gui.bgoulart@gmail.com>
2023-11-19 19:43:00 -0300

    init commit

---
+++ OrderEstimateService/src/main/java/pucrs/orderestimateservice/OrderEstimateServiceApplication.java
@@ -0,0 +1,13 @@
+package pucrs.orderestimateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEstimateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEstimateServiceApplication.class, args);
+	}
+
+}
---
+++ OrderEstimateService/src/test/java/pucrs/orderestimateservice/OrderEstimateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pucrs.orderestimateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEstimateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
