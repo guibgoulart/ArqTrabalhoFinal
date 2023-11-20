3ec327fd49b8cf7a0209d930b1adccc6a20d3715
Luiz Moraes <luiz.moraes@pensiondynamics.com.br>
2023-11-20 18:21:06 -0300

    feat: client ports configuration and kafka server

--- OrderEstimateService/src/main/java/pucrs/orderestimateservice/application/clients/CityClient.java
+++ OrderEstimateService/src/main/java/pucrs/orderestimateservice/application/clients/CityClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pucrs.orderestimateservice.application.dto.CityDTO;
 
-@FeignClient(name = "city-service", url = "http://localhost:8080") // Adjust the port accordingly
+@FeignClient(name = "city-service", url = "http://localhost:8082") // Adjust the port accordingly
 public interface CityClient {
     @GetMapping("/api/cities/name/{name}")
     CityDTO getCityByName(@PathVariable("name") String name);
--- OrderEstimateService/src/main/java/pucrs/orderestimateservice/application/clients/PromotionClient.java
+++ OrderEstimateService/src/main/java/pucrs/orderestimateservice/application/clients/PromotionClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pucrs.orderestimateservice.application.dto.PromotionDTO;
 
-@FeignClient(name = "promotion-service", url = "http://localhost:8080") // Adjust the port accordingly
+@FeignClient(name = "promotion-service", url = "http://localhost:8081") // Adjust the port accordingly
 public interface PromotionClient {
     @GetMapping("/api/promotions/{cityName}")
     PromotionDTO getActivePromotion(@PathVariable("cityName") String name);
